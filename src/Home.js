58b2e8e530090df97b060ab4222da849d62e8852
Данил Коротенко <dankorotenko@mbpro.local>
2021-05-14 18:25:08 +0200

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import BlogList from './BlogList';
+import useFetch from './useFetch';
+const Home = () => {
+    const { data, isPending, error } = useFetch('http://localhost:8000/blogs');
+    
+    return ( 
+        <div className="home">
+            { error && <div className="error">{error}</div>}
+            {isPending && <div>loading...</div>}
+            {data && <BlogList blogs={data} title="All Blogs"/> }
+        </div>
+     );
+}
+ 
+export default Home;
